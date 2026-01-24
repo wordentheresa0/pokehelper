43298a60f55cad35d291f566a68a1fa16980f520
Theresa Worden <wordentheresa0@gmail.com>
2026-01-24 11:04:59 -0500

    refamiliarizing

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <div className="app-container">
+      <h1>testing</h1>
+    </div>
   </StrictMode>,
-)
+);
