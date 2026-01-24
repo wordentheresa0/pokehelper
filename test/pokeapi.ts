1d6bb36b96eb2f960a8748ae82d0ed37b9f9dffb
Theresa Worden <wordentheresa0@gmail.com>
2026-01-24 01:03:51 -0500

    init commit

---
+++ test/pokeapi.ts
@@ -0,0 +1,19 @@
+import { PokemonClient, MoveClient, type Pokemon, type Move } from 'pokenode-ts';
+
+const pokemonAPI = new PokemonClient();
+const moveAPI = new MoveClient();
+
+// Getting the pokemon "Luxray"
+// Printing its moveset and the type of each move
+const pokemon = await pokemonAPI.getPokemonByName("luxray")
+    .then(async (response : Pokemon) => {
+        const moves = response.moves;
+        for (const move of moves) {
+            await moveAPI.getMoveByName(move.move.name)
+                .then((moveResponse : Move) => {
+                    console.log(moveResponse.name + ", " + moveResponse.type.name);
+                })
+        }
+    })
+    .catch((error : any) => console.log("Error: ", error));
+
