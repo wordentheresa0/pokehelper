351108e5c3570580fa1ed932b9c4cce5f75d5b68
Theresa Worden <wordentheresa0@gmail.com>
2026-02-21 15:05:27 -0500

    plus button grid

--- app/src/components/RightPanel/RightPanel.tsx
+++ app/src/components/RightPanel/RightPanel.tsx
@@ -1,9 +1,16 @@
 import PlusButton from "../PlusButton/PlusButton";
 import "./RightPanel.css";
-import "../PlusButton/PlusButton.css"
+import "../PlusButton/PlusButton.css";
 
 export default function RightPanel() {
-  return <div className="right-red-square">
-    <PlusButton />
-  </div>;
+  return (
+    <div className="right-red-square">
+      <PlusButton />
+      <PlusButton />
+      <PlusButton />
+      <PlusButton />
+      <PlusButton />
+      <PlusButton />
+    </div>
+  );
 }
